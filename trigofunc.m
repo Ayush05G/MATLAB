da2db0d995490bb47417ea44ff4a965d951e6f97
Ayush Gupta <ayush2425.rk@gmail.com>
2024-08-11 03:04:03 +0530

    trigofunction

---
+++ trigofunc.m
@@ -0,0 +1,21 @@
+% input value is x which is initialized 
+% as 34 
+x=34 ;
+
+% compute sin(x) 
+sin(x) 
+
+% compute sind(x) 
+sind(x) 
+
+% compute asin(x) 
+asin(x) 
+
+% compute asind(x) 
+asind(x) 
+
+% compute sinh(x) 
+sinh(x) 
+
+% compute asinh(x) 
+asinh(x)
